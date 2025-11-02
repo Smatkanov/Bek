355c1887e7d3a5b4cd205ac5f0388b5ce43a19b5
Smatkanov <begalysmatkanov703@gmail.com>
2025-11-02 13:23:53 +0300

    проект файл

---
+++ Begaly.py
@@ -0,0 +1,8 @@
+ihjefbvinnrunvirnfcska
+sd('/c;ed..;c;e.;v.c;e.f;c.ed;c.'
+   'd;c.;d.c;e.c;.e;c.;ed.c;.;xz;sx.;'
+   ''
+   'elf,jihbr4hbegujhsnhfbhernlvahguiroehguierh8934uguijerrjvnjnsdjvnjfdnvsjnjsnvjdndsvjndsjvndfjnvj'
+   'vfmfjkvluqihgfuiphriuphwuihgiughruirgjdsngjsdnbgouwguh[p;dvnren;jbnm\'
+   'idfjibkifjbierlu;abourdnbojk;jcxnbldkfxncbaoffbudo;fpnuncx .df '
+   'erkfdbkmrkfmbkmkamklx/cm//v';bmmlkksmfbkmkbmkdfmkmbbkmdfk)
